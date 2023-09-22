352784c23dfff06192457d1687ea0134aa52f2b8
secripite <keonho04@gmail.com>
2023-09-22 17:31:45 +0900

    commit

--- C/report/HW02/김건호_HW2_문제2.c
+++ C/report/HW02/김건호_HW2_문제2.c
@@ -1,8 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
+void frqtest (int x[]){
+    int Max=0,Max_num;
+
+    for(int i = 0; i < 10 ; i++){
+        if(x[i]>Max){
+            Max = x[i];
+            Max_num = i;
+        }
+    }
+    printf("0 ~ 9 중 가장 많이 나온 수는 %d 이고, %d번 나왔습니다.", Max_num,Max);
+}
+
+
 int main(){
-    
+    int memo[10]={0,};
+    int num;
+
     printf("0에서 9까지의 정수를 입력하시오.\n");
-    
+    for(int i = 0; i < 20 ; i++){
+        scanf("%d",&num);
+        memo[num]++;
+    }
+    frqtest(memo);
 }
