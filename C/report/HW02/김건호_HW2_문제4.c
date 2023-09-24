5d62ac508b176e186b612eb9983457922281260e
secripite <keonho04@gmail.com>
2023-09-25 01:56:52 +0900

    sum

--- C/report/HW02/김건호_HW2_문제4.c
+++ C/report/HW02/김건호_HW2_문제4.c
@@ -1,30 +1,62 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
+char sum_result[20],minus_result[20];
+
+char* sum(char* num1, int len1, char* num2, int len2){
+    int temp = 0;
+
+    do
+    {
+        if(len2 + 1){
+
+            sum_result[len1] = (*(num1 + len1) + *(num2 + len2) - 96 + temp) % 10 + 48;
+            if(*(num1 + len1) + *(num2 + len2) - 96 + temp >= 10)
+                temp = 1;
+            else
+                temp = 0;
+        }
+        else{
+            sum_result[len1] = (*(num1 + len1) - 48 + temp) % 10 + 48;
+            if(*(num1 + len1) + temp - 48 >= 10)
+                temp = 1;
+            else
+                temp = 0;
+        }
+
+        len1--;
+        len2--;
+    } while ((len1 + 1) || temp != 0);
+
+    printf("%s\n", sum_result);
+}
+
+char* minus(char* num1, char* num2){
+    
+}
+
 int main(){
-    char num1[20]={0,}, num2[20]={0,}, sum_result[20]={0,},minus_result[20]={0,};
-    int len_num1 = 0, len_num2 = 0, i=0, j=0;
+    char num1[20]={0,}, num2[20]={0,};
+    int len_num1 = 0, len_num2 = 0;
 
     printf("ют╥б : ");
     scanf("%s %s", num1, num2);
     
-    while(num1[i]!='\0'){
-        i++;
+    while(num1[len_num1]!='\0'){
         len_num1++;
     }
-
-    while(num2[j]!='\0'){
-        j++;
+    len_num1--;
+    while(num2[len_num2]!='\0'){
         len_num2++;
     }
+    len_num2--;
 
 
 
-    if(len_num1 > len_num2)
-        for(;)
-    else if(len_num1 < len_num2)
+    printf("гу : ");
+    if(len_num1 >= len_num2)
+        sum(num1, len_num1, num2, len_num2);
     else
-    
-    printf("%s %s", num1, num2);
+        sum(num2, len_num2, num1, len_num1);
 
 }
